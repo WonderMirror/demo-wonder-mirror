293661424b077c8104d3e1b3f5ebe0943fa242af
HimanshuKalper <himanshu.mittal@datakalp.com>
2021-10-10 21:17:08 +0530

    intial commit

---
+++ src/camera/setup_single_camera.py
@@ -0,0 +1,12 @@
+"""
+Describes the real life settings of the camera.
+"""
+
+# Camera height from the ground (in feets)
+cam_height_off_ground = 5
+
+# Feet height from the ground (in feets)
+feet_height_off_ground = 0.4
+
+# Horizontal distance between camera and feet
+hori_dist_cam_feet = 5
